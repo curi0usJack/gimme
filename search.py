952e9e5b407ca64cdddf3984bab924a6dc42ea9d
curi0usJack <curi0sjack@gmail.com>
2021-04-27 19:53:02 -0500

    Adding sendpush method and test PS5 url. (Currently broken)

--- search.py
+++ search.py
@@ -5,13 +5,26 @@ import json
 import random
 from pushover import Client
 
+def sendpush(message):
+    apikey = "<your key>"
+    userkey = "<user key>"
+    client = Client(userkey, api_token=apikey)
+    client.send_message(message)
+
 
 def search(url, searchstring):
-    ua = "Safari: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Safari/605.1.15"
-    headers = { 'User-Agent': ua }
+    ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.2 Safari/605.1.15"
+    headers = { 'User-Agent': ua,
+                'Set-Cookie': "SID=81f6103a-b720-4e03-aa59-6bfece800c84; path=/; domain=.bestbuy.com",
+                'Set-Cookie': "bby_cbc_lb=p-browse-e; expires=Wed, 28-Apr-2021 00:54:38 GMT; path=/; domain=.bestbuy.com",
+                'x-employment': "If you are reading this, consider a job at BestBuy.com http://www.bestbuy-jobs.com/job-family/all-corporate-careers/"
+            }
     resp = requests.get(url, headers=headers)
-    print(resp.status_code)
+    if resp.status_code == 200:
+        # Do our search
+    else:
+        # notify us
 
 
-url = "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p\\?skuId\\=6426149"
-search(url)
+url = "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"
+search(url, "Sold Out")
